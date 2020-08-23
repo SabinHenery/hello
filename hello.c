97c6148c2a46bc19bb4c226155efca040e2ce97d
Sabin Henery <shenery2@huskers.unl.edu>
2020-08-23 07:59:27 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Sabin Henery
+ *        shenery2@huskers.unl.edu
+ * Date: 2020/08/23
+ *
+ * A printout of my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sabin Henery!\n Undecided.\n");
+
+  return 0;
+}
